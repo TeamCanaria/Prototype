5af2a82ab538d58c7bed973296eab767a3bb56d8
Vinh Tran <vinh.tran92@gmail.com>
2018-03-28 15:21:10 +1000

    Implemented Fatigue Button

--- Canaria_Prototype_Uno/Canaria_Prototype_Uno.ino
+++ Canaria_Prototype_Uno/Canaria_Prototype_Uno.ino
@@ -11,11 +11,19 @@
 
 /************VARIABLES******************/
 //const int LED_PIN = 4;
-const int RED_LED = 16;
+//const int RED_LED = 4;
+const int RED_LED = 16; // A2 on Ardunio Nano
+const int IR_LED = 15; //A1 on Arduino Nano
+
 const int GREEN_LED = 5;
 const int ADC_PIN = 17; // A3 on Arduino Nano
+
+//const int BUTTON_INT = 14; // A0 on Ardunio Nano
+const int BUTTON_INT = 2; // D2 on Ardunio Nano
+
 uint32_t tsLastPollUs = 0;
 float volt = 0;
+volatile bool buttonPressed = false;
 /**************************************/
 
 /**********STRUCT*********************/
@@ -32,6 +40,12 @@ void setup() {
   Serial.begin(115200);
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
+  pinMode(IR_LED, OUTPUT);
+  
+
+  pinMode(BUTTON_INT, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON_INT), adc_rdy_isr, RISING);
   
 //  digitalWrite(RED_LED, 1);
 }
@@ -49,8 +63,7 @@ void powerSequence (void) {
 //  delayMicroseconds(75);
   /* LED2 starts conversion*/
   LedValues.led1 = get_led_val(ADC_PIN);
-//  digitalWrite(RED_LED, 1);
-//  delayMicroseconds(270);
+
 }
 /*******Main loop********/
 void loop() {
@@ -58,14 +71,20 @@ void loop() {
   
   powerSequence();
   
-
+  
   if (micros() < tsLastPollUs || micros() - tsLastPollUs > POLL_PERIOD_US) {
         
         tsLastPollUs = micros();        
         Serial.println(LedValues.led1);
-//          Serial.println("Hello");
+
 //        volt = (5.0/1023.0)*(LedValues.led1);
 //        Serial.println(volt);
     }
-//  delay(100);
 }
+
+void adc_rdy_isr(){
+  Serial.println("F");
+  delay(1);
+  buttonPressed = true;
+}
+
